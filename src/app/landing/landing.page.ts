740e101bc30927a7cef85a2c4d5b8778825d8421
JDgayagoy <jdgprofessional@gmail.com>
2025-05-31 23:43:03 +0800

    Landing ni Kat

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { createAnimation } from '@ionic/angular';
 
 @Component({
-  standalone: false,
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
+  standalone: false,
 })
-export class LandingPage implements OnInit {
+export class LandingPage {
+  touchStartY = 0;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    this.touchStartY = event.touches[0].clientY;
   }
 
+  @HostListener('touchend', ['$event'])
+  async onTouchEnd(event: TouchEvent) {
+    const touchEndY = event.changedTouches[0].clientY;
+    const swipeDistance = this.touchStartY - touchEndY;
+
+    if (swipeDistance > 100) {
+      await this.playTransition();
+      this.router.navigateByUrl('/login', { replaceUrl: true });
+    }
+  }
+
+  async playTransition() {
+    const animation = createAnimation()
+      .addElement(document.querySelector('ion-content')!)
+      .duration(500)
+      .easing('ease-in-out')
+      .fromTo('opacity', '1', '0')
+      .fromTo('transform', 'translateY(0)', 'translateY(-100px)');
+
+    await animation.play();
+  }
 }
