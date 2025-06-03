ff1e7fac8908f9b2d134213c5d9b8b4dc6221e24
butterkat <manuelakatrina12@gmail.com>
2025-06-03 12:58:10 +0800

    hehe

--- src/app/profile-pns/profile-pns.page.ts
+++ src/app/profile-pns/profile-pns.page.ts
@@ -82,7 +82,7 @@ export class ProfilePnsPage implements OnInit {
 
 
   goToProfileMethod() {
-    this.router.navigate(['/profile']);
+    this.router.navigate(['/landlord-profile']);
   }
 
   onProfileChange() {
