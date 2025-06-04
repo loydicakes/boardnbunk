ba2d05f8133fa9dbcdccc742345a28b30c395217
butterkat <manuelakatrina12@gmail.com>
2025-06-05 01:14:08 +0800

    mema

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -1,104 +1,104 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FirestoreService, Room } from '../services/firestore.service';
-import { ModalController } from '@ionic/angular';
-import { NotificationModalComponent } from '../notification-modal/notification-modal.component';
-@Component({
-  standalone: false,
-  selector: 'app-homepage',
-  templateUrl: './homepage.page.html',
-  styleUrls: ['./homepage.page.scss'],
-})
-export class HomepagePage implements OnInit {
-  rooms: Room[] = [];
-  filteredRooms: Room[] = [];
-  selectedType: string = '';
-  searchTerm: string = '';
-  currentUserId: string = '';
-  favoriteIds: Set<string> = new Set();
+  import { Component, OnInit } from '@angular/core';
+  import { Router } from '@angular/router';
+  import { FirestoreService, Room } from '../services/firestore.service';
+  import { ModalController } from '@ionic/angular';
+  import { NotificationModalComponent } from '../notification-modal/notification-modal.component';
+  @Component({
+    standalone: false,
+    selector: 'app-homepage',
+    templateUrl: './homepage.page.html',
+    styleUrls: ['./homepage.page.scss'],
+  })
+  export class HomepagePage implements OnInit {
+    rooms: Room[] = [];
+    filteredRooms: Room[] = [];
+    selectedType: string = '';
+    searchTerm: string = '';
+    currentUserId: string = '';
+    favoriteIds: Set<string> = new Set();
 
-  constructor(
-    private router: Router,
-    private firestoreService: FirestoreService,
-    private modalCtrl: ModalController
-  ) {}
+    constructor(
+      private router: Router,
+      private firestoreService: FirestoreService,
+      private modalCtrl: ModalController
+    ) {}
 
-  async ngOnInit() {
-    const user = localStorage.getItem('user');
-    if (user) {
-      const parsedUser = JSON.parse(user);
-      this.currentUserId = parsedUser.uid;
-    } else {
-      this.router.navigate(['/login']);
-      return;
-    }
+    async ngOnInit() {
+      const user = localStorage.getItem('user');
+      if (user) {
+        const parsedUser = JSON.parse(user);
+        this.currentUserId = parsedUser.uid;
+      } else {
+        this.router.navigate(['/login']);
+        return;
+      }
 
-    this.rooms = (await this.firestoreService.getAvailableRooms()) || [];
-    await this.loadUserFavorites();
-    this.filteredRooms = this.rooms.map(room => ({
-      ...room,
-      isFavorite: this.favoriteIds.has(room.id)
-    }));
-  }
-
-  async loadUserFavorites() {
-    const favorites = await this.firestoreService.getUserFavorites(this.currentUserId);
-    this.favoriteIds = new Set(favorites.map(fav => fav.id));
-  }
-
-  filterRooms(type: string) {
-    this.selectedType = type;
-    this.applyFilters();
-  }
-
-  applyFilters() {
-    this.filteredRooms = this.rooms
-      .filter(room => {
-        const matchesType = this.selectedType ? room.type === this.selectedType : true;
-        const matchesSearch = this.searchTerm
-          ? room.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-          : true;
-        return matchesType && matchesSearch;
-      })
-      .map(room => ({
+      this.rooms = (await this.firestoreService.getAvailableRooms()) || [];
+      await this.loadUserFavorites();
+      this.filteredRooms = this.rooms.map(room => ({
         ...room,
         isFavorite: this.favoriteIds.has(room.id)
       }));
-  }
-
-  async toggleFavorite(room: any) {
-    if (!this.currentUserId) return;
-
-    if (room.isFavorite) {
-      await this.firestoreService.removeRoomFromUserFavorites(this.currentUserId, room.id);
-      this.favoriteIds.delete(room.id);
-    } else {
-      await this.firestoreService.addRoomToUserFavorites(this.currentUserId, {
-        id: room.id,
-        name: room.name,
-        type: room.type,
-        price: room.price
-      });
-      this.favoriteIds.add(room.id);
     }
 
-    room.isFavorite = !room.isFavorite;
-  }
+    async loadUserFavorites() {
+      const favorites = await this.firestoreService.getUserFavorites(this.currentUserId);
+      this.favoriteIds = new Set(favorites.map(fav => fav.id));
+    }
 
-  async openNotifications() {
-    if (!this.currentUserId) return;
+    filterRooms(type: string) {
+      this.selectedType = type;
+      this.applyFilters();
+    }
 
-    const userDoc = await this.firestoreService.getDocument('users', this.currentUserId);
-    const notifications = userDoc?.['notifications'] || [];
+    applyFilters() {
+      this.filteredRooms = this.rooms
+        .filter(room => {
+          const matchesType = this.selectedType ? room.type === this.selectedType : true;
+          const matchesSearch = this.searchTerm
+            ? room.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+            : true;
+          return matchesType && matchesSearch;
+        })
+        .map(room => ({
+          ...room,
+          isFavorite: this.favoriteIds.has(room.id)
+        }));
+    }
 
-    const modal = await this.modalCtrl.create({
-      component: NotificationModalComponent,
-      componentProps: {
-        notifications: notifications
+    async toggleFavorite(room: any) {
+      if (!this.currentUserId) return;
+
+      if (room.isFavorite) {
+        await this.firestoreService.removeRoomFromUserFavorites(this.currentUserId, room.id);
+        this.favoriteIds.delete(room.id);
+      } else {
+        await this.firestoreService.addRoomToUserFavorites(this.currentUserId, {
+          id: room.id,
+          name: room.name,
+          type: room.type,
+          price: room.price
+        });
+        this.favoriteIds.add(room.id);
       }
-    });
 
-    await modal.present();
+      room.isFavorite = !room.isFavorite;
+    }
+
+    async openNotifications() {
+      if (!this.currentUserId) return;
+
+      const userDoc = await this.firestoreService.getDocument('users', this.currentUserId);
+      const notifications = userDoc?.['notifications'] || [];
+
+      const modal = await this.modalCtrl.create({
+        component: NotificationModalComponent,
+        componentProps: {
+          notifications: notifications
+        }
+      });
+
+      await modal.present();
+    }
+
   }
-
-}
